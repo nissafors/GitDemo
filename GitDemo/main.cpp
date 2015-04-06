f89f29f6ed9ff88c5526dbd37899bd0a2f5ad429
nissafors <aa.gpost@gmail.com>
2015-04-06 09:08:48 +0200

    Initial commit

---
+++ GitDemo/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	cout << "Git är ett versionshanteringsprogram som skapades 2005 "
+		<< "för att hantera källkoden till Linuxkärnan."
+		<< endl;
+}
